efcf89ea0284c62d31283279f7ef38cace692067
dazelpro <dazelpro@gmail.com>
2019-08-29 06:18:39 +0700

    DZP21

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var about = this.nextElementSibling;
+        if (about.style.display === "block") {
+            about.style.display = "none";
+        } else {
+            about.style.display = "block";
+        }
+    });
+}
+
+function openPage(pageName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
